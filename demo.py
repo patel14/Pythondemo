f879364fe686f902f8dc7fce4bbf7e29301607bb
yash <ynpatel111@gmail.com>
2021-03-27 13:41:28 -0500

    update by Y

--- demo.py
+++ demo.py
@@ -3,7 +3,10 @@ b = "hello"
 c = 4
 d = 5
 f = c+d
-print (f)
+print(f)
+print("Hello I have made changes in the code")
+print("Today is saturday")
+print("I am Y")
 
 print("{} {} {} {}".format("values are ",a, b, d))
 
--- demo2.py
+++ demo2.py
@@ -1,5 +1,9 @@
 a = "hello "
 b = "world"
 c = a + b
+d = 5
+f = 10
+g = d + f
+print(g)
 
 print("{}{}{}".format("Statement is ", a, b))
